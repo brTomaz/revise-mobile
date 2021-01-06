cc40f73caaeef2ded7c76c17989746eab43ed6ec
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2021-01-06 02:10:11 -0300

    refactor: add theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -2,7 +2,10 @@ const theme = {
   colors: {
     primary: '#2D3D88',
     secondary: '#2DC2C3',
-    mainBg: '#040923'
+    mainBg: '#040923',
+    white: '#FAFAFA',
+    black: '#030617',
+    error: '#F74446'
   },
   fonts: {
     bold: 'Quicksand-SemiBold',
