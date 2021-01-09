db5901764ced07db26d638ec6a8cf84edd674f36
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2021-01-09 18:40:39 -0300

    test: ensure render the Answer with white color before clicking

---
+++ src/__tests__/components/Answer/Answer.spec.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import '@testing-library/jest-native'
+
+import { Answer } from '@/components'
+import { renderWithTheme } from '@/helpers'
+import theme from '@/styles/theme'
+
+jest.mock('../../../../node_modules/react-native-toast-message/src/index')
+
+describe('<Answer />', () => {
+  it('should render the Answer with white color before clicking', () => {
+    const { getByText } = renderWithTheme(
+      <Answer text="any text" correctAnswerIndex={1} answerIndex={2} />
+    )
+
+    const answer = getByText(/any text/)
+
+    expect(answer).toHaveStyle({
+      color: theme.colors.white
+    })
+  })
+})
