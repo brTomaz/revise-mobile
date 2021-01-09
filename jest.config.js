51fee4d8e4d4b1442b6827988c3cbd427be14d2b
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2021-01-09 11:39:35 -0300

    chore: add jest-styled-components

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,9 @@
 module.exports = {
   preset: '@testing-library/react-native',
-  setupFilesAfterEnv: ['@testing-library/jest-native/extend-expect'],
+  setupFilesAfterEnv: [
+    '@testing-library/jest-native/extend-expect',
+    'jest-styled-components'
+  ],
+  setupFiles: ['./node_modules/react-native-gesture-handler/jestSetup.js'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node']
 }
