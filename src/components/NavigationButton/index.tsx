6cbe8f1af061f201c215fc83f4ffd55f8dd24a90
brTomaz <bruno.tomaz@aluno.ufop.edu.br>
2021-01-11 21:09:43 -0300

    refactor: add icon to props

--- src/components/NavigationButton/index.tsx
+++ src/components/NavigationButton/index.tsx
@@ -1,15 +1,19 @@
-import React from 'react'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons'
+import React, { ReactNode } from 'react'
 import { useNavigation } from '@react-navigation/native'
 
 import * as Styles from './styles'
 
 type NavigationButtonProps = {
   text: string
+  icon: ReactNode
   navigateTo: string
 }
 
-const NavigationButton = ({ text, navigateTo }: NavigationButtonProps) => {
+const NavigationButton = ({
+  text,
+  icon,
+  navigateTo
+}: NavigationButtonProps) => {
   const navigation = useNavigation()
 
   async function handleNavigation(): Promise<void> {
@@ -19,9 +23,7 @@ const NavigationButton = ({ text, navigateTo }: NavigationButtonProps) => {
   return (
     <Styles.NavigationButton onPress={handleNavigation}>
       <Styles.ButtonText>{text}</Styles.ButtonText>
-      <Styles.IconContainer>
-        <Icon name="chevron-right-circle" size={24} color="#2DC2C3" />
-      </Styles.IconContainer>
+      <Styles.IconContainer>{icon}</Styles.IconContainer>
     </Styles.NavigationButton>
   )
 }
